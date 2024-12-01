ca13499cce4cc80785537e0b47ea4efcc55db68a
nisekoi <nisekoi@qq.com>
2024-12-01 21:22:34 +0800

    项目初始化和配置

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} 引入类型定义文件*/
+export default {
+  darkMode: 'media',
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
