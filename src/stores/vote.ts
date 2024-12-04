1900d73c23145f2263b33623ed243e9a96bda7cd
nisekoi <nisekoi@qq.com>
2024-12-04 22:12:27 +0800

    添加用户信息的类型

--- src/stores/vote.ts
+++ src/stores/vote.ts
@@ -1,16 +1,22 @@
-import { ref, computed } from 'vue'
+import { ref } from 'vue'
 import { defineStore } from 'pinia'
 import axios from 'axios'
 
+type UserInfo = {
+  userId: number,
+  name: string,
+  avatar: string
+}
+
 export const useVoteStore = defineStore('vote', () => {
-  const user = ref(null)
+  const user = ref<null | UserInfo>(null)
 
   async function getUserInfo() {
     try {
       var userInfo = await axios.get('/account/current-user')
       user.value = userInfo.data.result
       // console.log(user.value)
-    } catch(e) {
+    } catch (e: any) {
       if (e.isAxiosError) {
         console.log('user no login.')
       } else {
