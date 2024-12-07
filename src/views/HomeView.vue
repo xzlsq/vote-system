06e1185e427f4c0dc7a18502ae0da8205b3e20f4
nisekoi <nisekoi@qq.com>
2024-12-07 17:12:59 +0800

    用vant4重构该页面的标签栏

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,38 +1,23 @@
 <script setup lang="ts">
-import { useRouter } from 'vue-router'
-import { onMounted } from 'vue';
-import { useVoteStore } from '@/stores/vote';
+import { Tabbar, TabbarItem } from 'vant';
 
-var router = useRouter()
-var voteStore = useVoteStore()
-
-onMounted(async () => {
-  // debugger
-  // try {
-  //   var user = await axios.get('/account/current-user')
-  //   voteStore.setUser(user)
-  //   console.log('a', a)
-  // } catch (e) {
-
-  // }
-})
 
 </script>
 
 <template>
   <main class="flex flex-col h-[100vh] w-full mx-auto">
     <RouterView class="grow" />
-    <div class="flex h-12 w-full">
-      <RouterLink to="/select-create"
-        class="[&.router-link-active]:text-green-500 basis-0 grow flex flex-col items-center bg-gray-200">
-        <span>➕</span>
-        <span>新建</span>
-      </RouterLink>
-      <RouterLink to="/me"
-        class="[&.router-link-active]:text-green-500 basis-0 grow flex flex-col items-center bg-gray-200">
-        <span>🧑</span>
-        <span>我的</span>
-      </RouterLink>
+    <div class="flex h-12 w-full !bg-gray-200">
+      <Tabbar route>
+        <TabbarItem to="/select-create" icon="plus"
+          class="[&.router-link-active]:text-green-500 basis-0 grow flex flex-col items-center bg-gray-200">
+          新建
+        </TabbarItem>
+        <TabbarItem to="/me" icon="user-o"
+          class="[&.router-link-active]:text-green-500 basis-0 grow flex flex-col items-center bg-gray-200">
+          我的
+        </TabbarItem>
+      </Tabbar>
     </div>
   </main>
 </template>
